4d125159c4b684b8325c25abf333c1c7ab498246
lljh1992 <134458007+lljh1992@users.noreply.github.com>
2023-06-12 19:20:00 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.10 ms, Memory: 64 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120822. 문자열 뒤집기/문자열 뒤집기.java
@@ -0,0 +1,7 @@
+class Solution {
+    public String solution(String my_string) {
+        StringBuffer str = new StringBuffer(my_string);
+        String answer = str.reverse().toString();
+        return answer;
+    }
+}
