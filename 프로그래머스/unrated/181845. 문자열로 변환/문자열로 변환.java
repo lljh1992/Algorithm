709ab01213fd7781819883bf7f76de60f48a9164
lljh1992 <134458007+lljh1992@users.noreply.github.com>
2023-06-14 18:04:04 +0900

    [unrated] Title: 문자열로 변환, Time: 0.04 ms, Memory: 78.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181845. 문자열로 변환/문자열로 변환.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String solution(int n) {
+        // 자바에서 정수를 문자열로 변환하는 방법
+        // String.valueOf()
+        // Integer.toString()
+        // String.format()
+        String answer = String.valueOf(n);
+        
+        return answer;
+    }
+}
