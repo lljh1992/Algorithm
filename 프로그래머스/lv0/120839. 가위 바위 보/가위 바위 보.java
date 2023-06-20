d7a54481774d7a8bb2d748178a38e4b2b449e54c
lljh1992 <134458007+lljh1992@users.noreply.github.com>
2023-06-20 12:06:31 +0900

    [level 0] Title: 가위 바위 보, Time: 3.53 ms, Memory: 75.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String solution(String rsp) {
+        String answer = "";
+        String[] a = rsp.split("");
+        
+        for (int i = 0; i < rsp.length(); i++) {
+            if (a[i].equals("0")) {
+                answer += "5";
+            } else if (a[i].equals("2")) {
+                answer += "0";
+            } else if (a[i].equals("5")) {
+                answer += "2";
+            }
+            }
+        return answer;
+    }
+}
