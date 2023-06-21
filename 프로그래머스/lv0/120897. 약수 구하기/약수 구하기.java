880176aa2447831624373adfeb96d6860dba0565
lljh1992 <134458007+lljh1992@users.noreply.github.com>
2023-06-21 14:49:33 +0900

    [level 0] Title: 약수 구하기, Time: 0.39 ms, Memory: 73.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120897. 약수 구하기/약수 구하기.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+class Solution {
+    public int[] solution(int n) {
+        int a = 0;
+
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                a++;
+            }
+        }
+        
+        int[] answer = new int[a];
+        
+        int k = 0;
+        
+        for (int i =1; i <= n; i++) {
+            if (n % i == 0) {
+                answer[k] = i;
+                k++;
+            }
+        }
+        Arrays.sort(answer);
+        return answer;
+    }
+}
