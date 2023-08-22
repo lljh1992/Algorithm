3951a79d0dd4d8050a7f28d21ed8493603bf6999
lljh1992 <134458007+lljh1992@users.noreply.github.com>
2023-08-22 22:04:42 +0900

    [level 0] Title: 안전지대, Time: 0.05 ms, Memory: 74.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120866. 안전지대/안전지대.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int solution(int[][] board) {
+        boolean[][] bomb = new boolean[board.length][board.length];
+        int[] dx = {0, 1, 1, 1, 0, -1, -1, -1};
+        int[] dy = {-1, -1, 0, 1, 1, 1, 0, -1};
+        
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board.length; j++) {
+                if (board[i][j] == 1) {
+                    bomb[i][j] = true;
+                    for (int n = 0; n < 8; n++) {
+                        int x = i + dx[n];
+                        int y = j + dy[n];
+                        if (x >= 0 && x < board.length && y >= 0 && y < board.length) {
+                            bomb[x][y] = true;
+                        }
+                    }
+                }
+            }
+        }
+        
+        int answer = 0;
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board.length; j++) {
+                if (!bomb[i][j]) {
+                    answer++;
+                }
+            }
+        }
+        return answer;
+    }
+}
